b99d1fba0eb77dcb07378d0707745cd2f405d854
Gaffgit <twitchgaff@gmail.com>
2026-09-09 01:11:42 +0300

    no feedback given if nothing is pressed, refactored some parts of the code and made statistics a table

--- osa 1/unicafe/src/App.jsx
+++ osa 1/unicafe/src/App.jsx
@@ -8,29 +8,46 @@ const Header = ({text}) => {
 
 const Button = ({onClick, text}) => {
   return (
-    <button onClick={onClick}>{text}</button>
+    <button onClick={onClick} style={{
+        backgroundColor: 'white',
+        border: 'none',
+        borderRadius: '4px',
+        cursor: 'pointer',
+        boxShadow: '0 2px 4px rgba(0, 0, 0, 0.2)'
+      }}>{text}</button>
   )
 }
 
-const Statistic = ({total, text}) => { 
+const StatisticLine = ({total, text}) => { 
   return (
-  <p>{text} {total}</p> 
+   <tr>
+    <td>{text}</td>
+    <td>{total}</td>
+   </tr>
   )
 }
 
 const Statistics = ({statistics}) => {
-  const average = (statistics[0].total - statistics[2].total)/statistics[3].total
-  const positive = statistics[0].total/statistics[3].total
+  const all = statistics[0].total + statistics[1].total + statistics[2].total
+
+  if(all === 0) {
+    return (
+      <p>No feedback given</p>
+    )
+  }
+
+  const average = (statistics[0].total - statistics[2].total)/all
+  const positive = (statistics[0].total/all)*100
 
   return (
-      <div>
-    <Statistic text={statistics[0].text} total={statistics[0].total}/>
-    <Statistic text={statistics[1].text} total={statistics[1].total}/>
-    <Statistic text={statistics[2].text} total={statistics[2].total}/>
-    <Statistic text={statistics[3].text} total={statistics[3].total}/>
-    <Statistic text="average" total={average}/>
-    <Statistic text="positive" total= {positive + ' %'}/>
-    </div>
+      <table>
+    <StatisticLine text={statistics[0].text} total={statistics[0].total}/>
+    <StatisticLine text={statistics[1].text} total={statistics[1].total}/>
+    <StatisticLine text={statistics[2].text} total={statistics[2].total}/>
+    <StatisticLine text="all" total={all}/>
+    <StatisticLine text="average" total={average}/>
+    <StatisticLine text="positive" total= {positive + ' %'}/>
+    </table>
   )
 }
 
@@ -39,7 +56,6 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const [all, setAll] = useState(0)
 
   const statistics = [
     {
@@ -54,31 +70,21 @@ const App = () => {
       text: 'bad',
       total: bad
     },
-    {
-      text: 'all',
-      total: all
-    },
   ]
 
   const handleGoodClick = () => {
   const updatedGoodClick = good + 1
-  const updatedAllClick = all + 1
   setGood(updatedGoodClick)
-  setAll(updatedAllClick)
   }
 
   const handleNeutralClick = () => {
   const updatedNeutralClick = neutral + 1
-  const updatedAllClick = all + 1
   setNeutral(updatedNeutralClick)
-  setAll(updatedAllClick)
   }
 
   const handleBadClick = () => {
   const updatedBadClick = bad + 1
-  const updatedAllClick = all + 1
   setBad(updatedBadClick)
-  setAll(updatedAllClick)
   }
 
   return (
@@ -88,6 +94,7 @@ const App = () => {
       <Button onClick={handleNeutralClick} text={statistics[1].text}/>
       <Button onClick={handleBadClick} text={statistics[2].text}/>
       <Header text="statistics"/>
+      
       <Statistics statistics={statistics}/>
     </div>
   )
