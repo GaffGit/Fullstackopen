497090cf81cc86c3419123e74458cdc74b9ceed9
Gaffgit <twitchgaff@gmail.com>
2026-09-08 19:38:36 +0300

    split the component Content into 3 part components 1.2

--- osa 1/kurssitiedot/src/App.jsx
+++ osa 1/kurssitiedot/src/App.jsx
@@ -6,13 +6,23 @@ const Header = (props) => {
 
 const Content = (props) => {
   return (
-    <p>{props.part} {props.exercise}</p>
+    <div>
+    <Part part={props.part1} exercise={props.exercise1}/>
+    <Part part={props.part2} exercise={props.exercise2}/>
+    <Part part={props.part3} exercise={props.exercise3}/>
+      </div>
   )
 }
 
 const Total = (props) => {
   return (
     <p>Number of exercises {props.total}</p>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>{props.part} {props.exercise}</p>
   )
 }
 
@@ -29,9 +39,9 @@ const App = () => {
   return (
     <div>
       <Header name={course}/>
-      <Content part={part1} exercise={exercises1}/>
-      <Content part={part2} exercise={exercises2}/>
-      <Content part={part3} exercise={exercises3}/>
+      <Content part1={part1} exercise1={exercises1}
+               part2={part2} exercise2={exercises2}
+               part3={part3} exercise3={exercises3}/>
       <Total total={exercises1 + exercises2 + exercises3}/>
     </div>
   )
