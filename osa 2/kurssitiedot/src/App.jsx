cd5c8414d7387cb85bd2532542fe7cb39f34c210
Gaffgit <twitchgaff@gmail.com>
2026-09-09 18:32:21 +0300

    osa 2 created and made kurssitiedot render with 1 component in app and works now when adding or removing courses

---
+++ osa 2/kurssitiedot/src/App.jsx
@@ -0,0 +1,63 @@
+const Header = (props) => {
+  return (
+    <h1>{props.course.name}</h1>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      {props.course.parts.map(part => 
+      <Part key={part.id} part={part.name} exercises={part.exercises}/>
+      )}
+      </div>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>{props.part} {props.exercises}</p>
+  )
+}
+
+const Course = (props) => {
+  return (
+    <div>
+      <Header course= {props.course}/>
+      <Content course = {props.course}/>
+    </div>
+  )
+}
+
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    id: 1,
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Course course= {course}/>
+    </div>
+  )
+}
+
+export default App
